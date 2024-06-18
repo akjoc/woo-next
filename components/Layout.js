29ef07be09f830089d2d2b4d47af210f5a8a7348
amansteg <amanjoshi@stegpearl.com>
2024-06-18 18:48:17 +0530

    till video 2

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Header from "./Header";
+
+const Layout = (props) => {
+  return (
+    <div>
+      <Head>
+        <title> Woocommere React App </title>
+        <link
+          rel="stylesheet"
+          href="https://bootswatch.com/5/flatly/bootstrap.min.css"
+        />
+      </Head>
+      <Header />
+      {props.children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/Nav.js
@@ -0,0 +1,35 @@
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarColor01">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Home
+                <span className="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
