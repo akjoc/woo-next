204ccdc920090aa3b6f2eb08b79f858c25423205
amansteg <amanjoshi@stegpearl.com>
2024-06-19 16:31:10 +0530

    till video 4

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import Layout from "../components/Layout";
+import clientConfig from "../client-config";
+import fetch from 'isomorphic-unfetch';
+import Product from "../components/Products";
 
-const Index = () => {
-  return <Layout />;
+const Index = ( props ) => {
+
+  const {products} = props;
+  return (
+    <Layout>
+        {
+          products.length ? (
+            products.map( (product) => <Product key={product.id} product = { product }/>)
+          ) : ''
+        }
+    </Layout>
+  )
+};
+
+Index.getInitialProps = async () => {
+  const res = await fetch(`${clientConfig.siteUrl}/getProducts`);
+  const productsData =  await res.json();
+  return{
+    products: productsData
+  }
 };
 
 export default Index;
---
+++ wooConfig.js
@@ -0,0 +1,7 @@
+const wooConfig = {
+    siteUrl: 'https://woodmart.epp.solar/',
+    consumerKey: 'ck_9c044d109a661bcc2b86010dbed4a304e45d0cb1',
+    consumerSecret: 'cs_885a4f5b47b62054cae588d411e62521f771e186'
+};
+
+module.exports = wooConfig;
